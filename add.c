8c2949509749c439f8a42b99e51110894f842a64
navinyabharathi-it19 <54268273+navinyabharathi-it19@users.noreply.github.com>
2019-11-14 11:20:21 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter a value:");
+scanf("%d",&a);
+printf("enter b value:");
+scanf("%d",&b);
+if(a>b)
+{
+printf("%d is greater",a);
+}
+else
+{
+printf("%d is greater",b);
+}
+}
